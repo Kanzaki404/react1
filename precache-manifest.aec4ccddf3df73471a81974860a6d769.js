4d210a592612ff611d8e2806a37afaaaf1b3d0bb
Alexander Risteski <alexander.risteski23@gmail.com>
2020-05-12 13:09:39 +0200

    Updates

---
+++ precache-manifest.aec4ccddf3df73471a81974860a6d769.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "917a3d6e9b61ba60ec1cb2f8fc5f9352",
+    "url": "/ReactAssig1/index.html"
+  },
+  {
+    "revision": "ad65a5fd8c76e9232510",
+    "url": "/ReactAssig1/static/css/main.46bac420.chunk.css"
+  },
+  {
+    "revision": "b9225becff18e04d2cae",
+    "url": "/ReactAssig1/static/js/2.e58b66c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactAssig1/static/js/2.e58b66c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad65a5fd8c76e9232510",
+    "url": "/ReactAssig1/static/js/main.6fa14ac5.chunk.js"
+  },
+  {
+    "revision": "6e2f184e71be367dc606",
+    "url": "/ReactAssig1/static/js/runtime-main.b50da0a6.js"
+  },
+  {
+    "revision": "cbc6f5f6729f5fa15766e53eab69a282",
+    "url": "/ReactAssig1/static/media/background.cbc6f5f6.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/ReactAssig1/static/media/logo.ee7cd8ed.svg"
+  }
+]);
